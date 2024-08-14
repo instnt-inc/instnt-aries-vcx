a7640d7f79bdc84225f4f96256a242456fcf6e3c
Pankaj Singh <pankaj@instnt.org>
2024-08-14 21:52:02 +0530

    Added Steps to run udl file for added function

--- aries_framework_vcx/src/lib.rs
+++ aries_framework_vcx/src/lib.rs
@@ -1,10 +1,16 @@
-#![deny(clippy::unwrap_used)]
-uniffi_macros::include_scaffolding!("aries_framework_vcx");
+uniffi::include_scaffolding!("aries_framework_vcx");
 
+pub use crate::framework::add;// whatever we are going to expose to UDL step1
+//pub use crate::connection_service::ConnectionServiceConfig;
+
+/** below line will not make any impact if we added---step2 */
 pub use aries_vcx;
 pub use aries_vcx::aries_vcx_wallet::wallet::askar::askar_wallet_config::AskarWalletConfig;
 pub use url::Url;
+/**    --------- */
 pub mod framework {
+
+    /** step3 */
     use std::{
         error::Error,
         sync::{mpsc::Receiver, Arc, Mutex},
@@ -22,87 +28,96 @@ pub mod framework {
     use did_resolver_registry::ResolverRegistry;
     use log::{info, warn};
     use url::Url;
+    // commented as we dont have connection_service and invitation_service in root
+    // use crate::{
+    //     connection_service::{ConnectionService, ConnectionServiceConfig},
+    //     invitation_service::InvitationService,
+    // };
 
-    use crate::{
-        connection_service::{ConnectionService, ConnectionServiceConfig},
-        invitation_service::InvitationService,
-    };
+    /** step3 */
 
-    pub static IN_MEMORY_DB_URL: &str = "sqlite://:memory:";
-    pub static DEFAULT_WALLET_PROFILE: &str = "aries_framework_vcx_default";
+       pub static IN_MEMORY_DB_URL: &str = "sqlite://:memory:";
+       pub static DEFAULT_WALLET_PROFILE: &str = "aries_framework_vcx_default";
+
+       /** step1 */
+        pub fn add(a: u32, b: u32) -> String {
+            IN_MEMORY_DB_URL.to_string()
+        }
+
+    /** step4 */  
     pub static DEFAULT_ASKAR_KEY_METHOD: KeyMethod = KeyMethod::DeriveKey {
         inner: AskarKdfMethod::Argon2i {
             inner: (ArgonLevel::Interactive),
         },
     };
 
-    #[derive(Clone)]
-    pub struct FrameworkConfig {
-        pub wallet_config: AskarWalletConfig,
-        pub connection_service_config: ConnectionServiceConfig,
-        pub agent_endpoint: Url,
-        pub agent_label: String,
-    }
+    // #[derive(Clone)]
+    // pub struct FrameworkConfig {
+    //     pub wallet_config: AskarWalletConfig,
+    //    // pub connection_service_config: ConnectionServiceConfig,
+    //     pub agent_endpoint: Url,
+    //     pub agent_label: String,
+    // }
 
-    pub struct AriesFrameworkVCX {
-        pub framework_config: FrameworkConfig,
-        pub wallet: Arc<AskarWallet>,
-        pub did_resolver_registry: Arc<ResolverRegistry>,
-        pub invitation_service: Arc<Mutex<InvitationService>>,
+//     pub struct AriesFrameworkVCX {
+//         pub framework_config: FrameworkConfig,
+//         pub wallet: Arc<AskarWallet>,
+//         pub did_resolver_registry: Arc<ResolverRegistry>,
+//         pub invitation_service: Arc<Mutex<InvitationService>>,
 
-        /// A service for the management of any and all things related to connections, including the usage of invitations (Out Of Band Invitations), the DID Exchange protocol, and mediation protocols.
-        ///
-        /// Note: This is service is about generic DIDComm connections and is **NOT** to be confused with the specific Aries handshake connection protocol RFC 0160 - https://github.com/hyperledger/aries-rfcs/tree/main/features/0160-connection-protocol
-        pub connection_service: Arc<Mutex<ConnectionService>>,
-    }
+//         /// A service for the management of any and all things related to connections, including the usage of invitations (Out Of Band Invitations), the DID Exchange protocol, and mediation protocols.
+//         ///
+//         /// Note: This is service is about generic DIDComm connections and is **NOT** to be confused with the specific Aries handshake connection protocol RFC 0160 - https://github.com/hyperledger/aries-rfcs/tree/main/features/0160-connection-protocol
+//         pub connection_service: Arc<Mutex<ConnectionService>>,
+//     }
 
-    impl AriesFrameworkVCX {
-        pub async fn initialize(framework_config: FrameworkConfig) -> Result<Self, Box<dyn Error>> {
-            info!("Initializing Aries Framework VCX");
+//     impl AriesFrameworkVCX {
+//         pub async fn initialize(framework_config: FrameworkConfig) -> Result<Self, Box<dyn Error>> {
+//             info!("Initializing Aries Framework VCX");
 
-            // Warn if the wallet pass key being used is the sample key from the documentation
-            if framework_config.wallet_config.pass_key() == "sample_pass_key" {
-                warn!("The Default Wallet Pass Key SHOULD NOT be used in production");
-            }
+//             // Warn if the wallet pass key being used is the sample key from the documentation
+//             if framework_config.wallet_config.pass_key() == "sample_pass_key" {
+//                 warn!("The Default Wallet Pass Key SHOULD NOT be used in production");
+//             }
 
-            // Wallet Initialization
-            let wallet = Arc::new(framework_config.wallet_config.create_wallet().await?);
+//             // Wallet Initialization
+//             let wallet = Arc::new(framework_config.wallet_config.create_wallet().await?);
 
-            // DID Resolver
-            // TODO - DID Sov Resolver
-            let did_peer_resolver = PeerDidResolver::new();
-            let did_resolver_registry = Arc::new(
-                ResolverRegistry::new().register_resolver("peer".into(), did_peer_resolver),
-            );
+//             // DID Resolver
+//             // TODO - DID Sov Resolver
+//             let did_peer_resolver = PeerDidResolver::new();
+//             let did_resolver_registry = Arc::new(
+//                 ResolverRegistry::new().register_resolver("peer".into(), did_peer_resolver),
+//             );
 
-            // Service Initializations
-            let invitation_service = Arc::new(Mutex::new(InvitationService::new(
-                framework_config.clone(),
-                wallet.clone(),
-            )));
-            let connection_service = Arc::new(Mutex::new(ConnectionService::new(
-                framework_config.clone(),
-                wallet.clone(),
-                did_resolver_registry.clone(),
-                invitation_service.clone(),
-            )));
+//             // Service Initializations
+//             let invitation_service = Arc::new(Mutex::new(InvitationService::new(
+//                 framework_config.clone(),
+//                 wallet.clone(),
+//             )));
+//             let connection_service = Arc::new(Mutex::new(ConnectionService::new(
+//                 framework_config.clone(),
+//                 wallet.clone(),
+//                 did_resolver_registry.clone(),
+//                 invitation_service.clone(),
+//             )));
 
-            Ok(Self {
-                framework_config,
-                wallet,
-                did_resolver_registry,
-                invitation_service,
-                connection_service,
-            })
-        }
-    }
+//             Ok(Self {
+//                 framework_config,
+//                 wallet,
+//                 did_resolver_registry,
+//                 invitation_service,
+//                 connection_service,
+//             })
+//         }
+//     }
 
-    // TODO - Consider adding a way to register event emitters with restrictions on the type of events to listen to for a given emitter -- such as, only receive events for did-exchange response messages (rather than having to filter all events)
-    pub trait EventEmitter {
-        type Event;
-        fn emit_event(&mut self, event: Self::Event);
-        fn register_event_receiver(&mut self) -> Receiver<Self::Event>;
-    }
+//     // TODO - Consider adding a way to register event emitters with restrictions on the type of events to listen to for a given emitter -- such as, only receive events for did-exchange response messages (rather than having to filter all events)
+//     pub trait EventEmitter {
+//         type Event;
+//         fn emit_event(&mut self, event: Self::Event);
+//         fn register_event_receiver(&mut self) -> Receiver<Self::Event>;
+//     }
 }
 
 // pub mod connection_service {
