4e9f333f224820a537959bdb2d4c84f3c019eeb0
abhishek <abhishek@instnt.org>
2024-09-03 10:41:05 +0530

    Add script and GitHub Actions workflow to update datetime file. Testing hook 2

--- create_datetime_file.sh
+++ create_datetime_file.sh
@@ -9,7 +9,7 @@ CURRENT_DATETIME=$(date +"%Y-%m-%d %H:%M:%S")
 # Write the current date and time to the file
 echo "$CURRENT_DATETIME" > $FILE_PATH
 
-echo "Created $FILENAME with current date and time."
+echo "Created $FILE_PATH with current date and time."
 
 # Optionally, you could commit the changes to the repository
 #git add $FILE_PATH
