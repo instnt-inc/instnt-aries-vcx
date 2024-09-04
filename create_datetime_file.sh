26e762186cb26e0cab016965f145dd7d351efd97
abhishek <abhishek@instnt.org>
2024-09-04 18:47:25 +0530

    testing github actions

--- create_datetime_file.sh
+++ create_datetime_file.sh
@@ -12,7 +12,11 @@ echo "$CURRENT_DATETIME" > $FILE_PATH
 echo "Created $FILE_PATH with current date and time."
 
 # Optionally, you could commit the changes to the repository
-#git add $FILE_PATH
-#git commit -m "Update datetime.txt with current date and time"
+
+git config --global user.name "abhishek"
+git config --global user.email "abhishek@instnt.org"
+
+git add $FILE_PATH
+git commit -m "Update datetime.txt with current date and time"
 
 #testing
